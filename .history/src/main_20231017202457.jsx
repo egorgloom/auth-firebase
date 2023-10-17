0e18c6dce6243b5d4cee74e89b3c9dd81f117c58
EgorGL00MI <gitstage0@yandex.ru>
2023-10-17 20:29:33 +0300

    feat: add files

---
+++ .history/src/main_20231017202457.jsx
@@ -0,0 +1,39 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Protected from './components/Protected';
+
+import Login from './pages/Login';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import { store } from './store';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="signup" element={<SignUp />} />
+      <Route path="login" element={<Login />} />
+      <Route path="/" element={<Protected />} >
+        <Route path="/" index element={<Home />} />
+      </Route>
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+<RouterProvider router={router} />
+
+  </Provider>
+  
+);
---
+++ .history/src/main_20231017202731.jsx
@@ -0,0 +1,32 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Protected from './components/Protected';
+
+import Login from './pages/Login';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="signup" element={<SignUp />} />
+      <Route path="login" element={<Login />} />
+      <Route path="/" element={<Protected />} >
+        <Route path="/" index element={<Home />} />
+      </Route>
+    </Route>
+  )
+);
+
+
